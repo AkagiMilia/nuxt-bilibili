0f2daaae8ef7d804e3701e959f971e7f1c24cd51
AkagiMilia <zexinyang@163.com>
2023-11-03 16:53:50 +0800

    Init SEO

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
-<script setup>
-
+<script setup lang="ts">
+// SEO Optimization
+useSeoMeta({
+  title: '哔哩哔哩 (゜-゜)つロ 干杯~-bilibili',
+  description: '哔哩哔哩（bilibili.com)是国内知名的视频弹幕网站，这里有及时的动漫新番，活跃的ACG氛围，有创意的Up主。大家可以在这里找到许多欢乐。'
+})
 </script>
 
 <template>
@@ -9,3 +13,12 @@
     <nuxt-page/>
   </div>
 </template>
+
+<style lang="scss">
+:root {
+  /*设置主题色*/
+  --van-primary-color: #FB7299 !important;
+  --van-back-top-background: #fbfbfb !important;
+  --van-back-top-text-color: #666 !important;
+}
+</style>
