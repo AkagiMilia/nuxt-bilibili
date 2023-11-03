4bab593c0b4d7984c319fea6652d78329a3b2be8
AkagiMilia <zexinyang@163.com>
2023-11-03 14:18:11 +0800

    Init Router

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
+<script setup>
+
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <nuxt-link to="/">Home</nuxt-link><span>__</span>
+    <nuxt-link to="/video">Video</nuxt-link>
+    <nuxt-page/>
   </div>
 </template>
---
+++ pages/video/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h2> Video </h2>
+</template>
+
+<style scoped>
+
+</style>
